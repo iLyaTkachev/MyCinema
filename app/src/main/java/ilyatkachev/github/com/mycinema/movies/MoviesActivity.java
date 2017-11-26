fd0bfa8a96e066b61a7d57559a64899be666b55b
Ilya Tkachev <ilya.tkachev97@gmail.com>
2017-11-26 21:40:12 +0300

    navigation prepared

--- app/src/main/java/ilyatkachev/github/com/mycinema/movies/MoviesActivity.java
+++ app/src/main/java/ilyatkachev/github/com/mycinema/movies/MoviesActivity.java
@@ -1,15 +1,136 @@
 package ilyatkachev.github.com.mycinema.movies;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import ilyatkachev.github.com.mycinema.R;
 
 public class MoviesActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up the custom toolbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the navigation drawer.
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+
+        NavigationView nvDrawer = (NavigationView) findViewById(R.id.nav_view);
+        if (nvDrawer != null) {
+            setupDrawerContent(nvDrawer);
+        }
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        //Fragment fragment = null;
+        //Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.movies_navigation_menu_item:
+                Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+                //fragmentClass = FirstFragment.class;
+                break;
+            case R.id.tv_shows_navigation_menu_item:
+                //fragmentClass = SecondFragment.class;
+                break;
+            case R.id.people_navigation_menu_item:
+                //fragmentClass = ThirdFragment.class;
+                break;
+            case R.id.favourites_navigation_menu_item:
+                //fragmentClass = ThirdFragment.class;
+                break;
+            case R.id.my_lists_navigation_menu_item:
+                //fragmentClass = ThirdFragment.class;
+                break;
+            case R.id.account_navigation_menu_item:
+                //fragmentClass = ThirdFragment.class;
+                break;
+            case R.id.advanced_navigation_menu_item:
+                //fragmentClass = ThirdFragment.class;
+                break;
+            default:
+                //fragmentClass = FirstFragment.class;
+        }
+
+        try {
+            //fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+        //fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawerLayout.closeDrawers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }*/
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
