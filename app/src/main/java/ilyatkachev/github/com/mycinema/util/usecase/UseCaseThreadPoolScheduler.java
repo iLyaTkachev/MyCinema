4dbed1a5b356ad0600512c52aec9b7c21919b1a8
Ilya Tkachev <ilya.tkachev97@gmail.com>
2018-01-09 21:11:46 +0300

    UseCaseThreadPoolScheduler class implemented

--- app/src/main/java/ilyatkachev/github/com/mycinema/util/usecase/UseCaseThreadPoolScheduler.java
+++ app/src/main/java/ilyatkachev/github/com/mycinema/util/usecase/UseCaseThreadPoolScheduler.java
@@ -1,5 +1,11 @@
 package ilyatkachev.github.com.mycinema.util.usecase;
 
+import android.os.Handler;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import ilyatkachev.github.com.mycinema.util.usecase.UseCaseScheduler;
 
 /**
@@ -7,4 +13,44 @@ import ilyatkachev.github.com.mycinema.util.usecase.UseCaseScheduler;
  */
 public class UseCaseThreadPoolScheduler implements UseCaseScheduler {
 
+    private final Handler mHandler = new Handler();
+
+    public static final int POOL_SIZE = 2;
+
+    public static final int MAX_POOL_SIZE = 4;
+
+    public static final int TIMEOUT = 30;
+
+    private ThreadPoolExecutor mThreadPoolExecutor;
+
+    public UseCaseThreadPoolScheduler() {
+        mThreadPoolExecutor = new ThreadPoolExecutor(POOL_SIZE, MAX_POOL_SIZE, TIMEOUT, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(POOL_SIZE));
+    }
+
+    @Override
+    public void execute(Runnable pRunnable) {
+        mThreadPoolExecutor.execute(pRunnable);
+    }
+
+    @Override
+    public <V extends UseCase.ResponseValue> void notifyResponse(final V pResponse, final UseCase.UseCaseCallback<V> pUseCaseCallback) {
+        mHandler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                pUseCaseCallback.onSuccess(pResponse);
+            }
+        });
+    }
+
+    @Override
+    public <V extends UseCase.ResponseValue> void onError(final UseCase.UseCaseCallback<V> pUseCaseCallback) {
+        mHandler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                pUseCaseCallback.onError();
+            }
+        });
+    }
 }
