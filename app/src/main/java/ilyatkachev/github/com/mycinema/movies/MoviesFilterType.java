4c111dbe210a23e1b115874419a8f15578a90fbd
Ilya Tkachev <ilya.tkachev97@gmail.com>
2018-01-10 14:22:33 +0300

    Get Movies UseCase implemented

---
+++ app/src/main/java/ilyatkachev/github/com/mycinema/movies/MoviesFilterType.java
@@ -0,0 +1,5 @@
+package ilyatkachev.github.com.mycinema.movies;
+
+public enum MoviesFilterType {
+    POPULAR, TOP_RATED, UPCOMING, NOW_PLAYING;
+}
--- app/src/main/java/ilyatkachev/github/com/mycinema/movies/domain/usecase/GetMovies.java
+++ app/src/main/java/ilyatkachev/github/com/mycinema/movies/domain/usecase/GetMovies.java
@@ -2,7 +2,12 @@ package ilyatkachev.github.com.mycinema.movies.domain.usecase;
 
 import android.support.annotation.NonNull;
 
+import java.util.List;
+
 import ilyatkachev.github.com.mycinema.data.CinemaRepository;
+import ilyatkachev.github.com.mycinema.data.ICinemaDataSource;
+import ilyatkachev.github.com.mycinema.movies.MoviesFilterType;
+import ilyatkachev.github.com.mycinema.movies.domain.model.Movie;
 import ilyatkachev.github.com.mycinema.util.usecase.UseCase;
 
 public class GetMovies extends UseCase<GetMovies.RequestValues, GetMovies.ResponseValue> {
@@ -14,15 +19,50 @@ public class GetMovies extends UseCase<GetMovies.RequestValues, GetMovies.Respon
     }
 
     @Override
-    protected void executeUseCase(final RequestValues requestValues) {
+    public void executeUseCase(final RequestValues pRequestValues) {
+        mCinemaRepository.getMovies(pRequestValues.getPage(), new ICinemaDataSource.LoadMoviesCallback() {
 
+            @Override
+            public void onMoviesLoaded(List<Movie> pMovies) {
+                getUseCaseCallback().onSuccess(new ResponseValue(pMovies));
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                getUseCaseCallback().onError();
+            }
+        });
     }
 
-    public static final class RequestValues implements UseCase.RequestValues{
+    public static final class RequestValues implements UseCase.RequestValues {
 
+        private final int mPage;
+        private final MoviesFilterType mMoviesFilterType;
+
+        public RequestValues(int pPage, @NonNull final MoviesFilterType pMoviesFilterType) {
+            mPage = pPage;
+            mMoviesFilterType = pMoviesFilterType;
+        }
+
+        public int getPage() {
+            return mPage;
+        }
+
+        public MoviesFilterType getMoviesFilterType() {
+            return mMoviesFilterType;
+        }
     }
 
-    public static final class ResponseValue implements UseCase.ResponseValue{
+    public static final class ResponseValue implements UseCase.ResponseValue {
 
+        private final List<Movie> mMovies;
+
+        public ResponseValue(@NonNull final List<Movie> pMovies) {
+            mMovies = pMovies;
+        }
+
+        public List<Movie> getMovies() {
+            return mMovies;
+        }
     }
 }
